39aa742e19c0e153d6131d66209d789c773aaf6f
JoniPS\Joni <Doriandjo@gmail.com>
2017-04-18 19:48:35 +0400

    Чистка мусора

--- k1.cpp
+++ k1.cpp
@@ -2,8 +2,9 @@
 
 K1::K1()
 {
-connect(this ,&K1::getCharacters,this,&K1::onGetCharacters);
-connect(this, &K1::finished, &MyThread, &QThread::quit);
+connect (this ,&K1::getCharacters,this,&K1::onGetCharacters);
+connect (this, &K1::finished, &MyThread, &QThread::quit);
+
 this->moveToThread(&MyThread);
 MyThread.start();
 }
@@ -15,10 +16,10 @@ void K1::onGetCharacters(const QString &str)
      int length;
      if(file.open(QIODevice::ReadOnly)) {
         length = file.size();
+        file.close();
      } else {
         length = 0;
      }
-     file.close();
      emit display(length);
      emit finished();
 }
--- main.cpp
+++ main.cpp
@@ -1,37 +1,17 @@
-//#include <QCoreApplication>
 #include<QApplication>
 #include <QDebug>
 #include <iterator>
 #include <QLCDNumber>
 #include <QThread>
-//#include "buf.h"
 #include "K1.h"
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-   // VectorMt<double>  p;
-    /*for(int i = 0; i <6; i++) {
-        double   n = i*5.3;
-        p.push_back(n);
-    }
-    p.deleteElement(3);
-
-    qDebug() <<p[3];
-
-    list<double> v(p.begin(),p.end());
-
-    for(list<double>::iterator it = v.begin(); it != v.end(); ++it) {
-        qDebug() <<*it;
-    }
-    qDebug() <<"++++++++++++++++++++++";
-   for(int i = 0;i<15;i++) {
-        qDebug() <<++p;
-    }*/
     QLCDNumber  lsd;
-    K1 p;
-    QObject::connect(&p, SIGNAL(display(int)), &lsd, SLOT(display(int)));
+    K1 loadFile;
+    QObject::connect(&loadFile, SIGNAL(display(int)), &lsd, SLOT(display(int)));
     lsd.setSegmentStyle(QLCDNumber::Filled);
     lsd.display(10);
     lsd.resize(220,90);
@@ -39,6 +19,6 @@ int main(int argc, char *argv[])
 
     qDebug() <<QThread::currentThreadId();
     QString Directory = "D:\\123.txt";
-    emit p.getCharacters(Directory);
+    loadFile.getCharacters(Directory);
     return a.exec();
 }
